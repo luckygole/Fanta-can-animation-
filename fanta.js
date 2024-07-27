adb2d024c25212bc738f9996e1c582cd1c969f0e
bhumika sahu <bhumi16124@gmail.com>
2024-07-27 22:46:57 +0530

    fanta.js file is saved in git

---
+++ fanta.js
@@ -0,0 +1,89 @@
+var tl = gsap.timeline({
+    duration:1,
+    // delay:2,
+    scrollTrigger : {
+    // scroller: "body" ,
+    trigger: ".two",
+    start: "0% 95%",
+    end: "70% 50%",
+    
+    scrub: true,
+    // markers:true,
+}})
+
+tl.to("#fanta" , {
+    top:"120%",
+    left: "0%"
+} , 'orange')
+
+tl.to("#orange-cut" , {
+    top:"160%",
+    left: "23%"
+} , 'orange')
+
+tl.to("#orange" , {
+    width:"15%",
+    top:"160%",
+    right: "10%"
+} , 'orange')
+
+tl.to("#leaf" , {
+    top:"110%",
+    rotate:"130deg",
+    left: "70%"
+} , 'orange')
+tl.to("#leaf2" , {
+    top:"110%",
+    left: "0%"
+} , 'orange')
+
+var tl2 = gsap.timeline({
+    duration:2,
+    // delay:3,
+    scrollTrigger : {
+    // scroller: "body" ,
+    trigger: ".three",
+    start: "0% 95%",
+    end: "20% 50%",
+    
+    scrub: true,
+    // markers:true,
+}})
+
+tl2.from( ".lemon1" , {
+    rotate:"-90deg",
+    left:"-100%",
+    top:"110%"
+} , 'ca')
+
+tl2.from( "#sprite" , {
+    rotate:"-90deg",
+    left:"-100%",
+    top:"110%"
+} , 'ca')
+
+tl2.from( ".lemon2" , {
+    rotate:"90deg",
+    left:"100%",
+    top:"110%"
+} , 'ca')
+
+tl2.from( "#pepsi" , {
+    rotate:"90deg",
+    left:"100%",
+    top:"110%"
+} , 'ca')
+
+tl2.to( "#orange-cut" , {
+    width:"18%",
+    left:"42%",
+    top:"200%",
+} , 'ca')
+
+tl2.to( "#fanta" , {
+    width:"35%",
+    left:"33%",
+    top:"205%"
+} , 'ca')
+
+
